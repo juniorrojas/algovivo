72165ce67023bdc7277a3f2e72b29be278dd28dd
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-22 02:16:21 -0500

    WIP

--- algovivo/mmgrten/index.js
+++ algovivo/mmgrten/index.js
@@ -1,14 +1,14 @@
 const Engine = require("./Engine");
 
-async function engine(args = {}) {
-  const ten = new Engine({
-    wasmInstance: args.wasmInstance
-  });
-  return ten;
-}
+// async function engine(args = {}) {
+//   const ten = new Engine({
+//     wasmInstance: args.wasmInstance
+//   });
+//   return ten;
+// }
 
 module.exports = {
-  engine: engine,
+  // engine: engine,
   Engine: Engine,
   Tensor: require("./Tensor"),
   mmgr: require("./mmgr"),
