dbe6a67dc8a057ff0c49e4e5cfc23134f3369253
Junior Rojas <jrojasdavalos@gmail.com>
2023-04-28 12:16:05 -0500

    Refactor build script (#10)

--- build.sh
+++ build.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-clang=${llvm_bin_dir}/clang++
-llc=${llvm_bin_dir}/llc
-ld=${llvm_bin_dir}/wasm-ld
-opt=${llvm_bin_dir}/opt
+clang=${LLVM_BIN_DIR}/clang++
+llc=${LLVM_BIN_DIR}/llc
+ld=${LLVM_BIN_DIR}/wasm-ld
+opt=${LLVM_BIN_DIR}/opt
 
-src_filename=csrc/main.cpp
-ll_filename=main.out.ll
-ll_diff_filename=main.diff.out.ll
-ll_diff_opt_filename=main.diff.opt.out.ll
-o_filename=main.out.o
-wasm_filename=build/algovivo.wasm
-mkdir -p build
+src_filename="csrc/main.cpp"
+lib_name="algovivo"
+build_dirname=build
 
-echo "building wasm..." && \
+ll_filename="${lib_name}.out.ll"
+ll_diff_filename="${lib_name}.diff.out.ll"
+ll_diff_opt_filename="${lib_name}.diff.opt.out.ll"
+o_filename="${lib_name}.out.o"
+wasm_filename=${build_dirname}/algovivo.wasm
+
+mkdir -p ${build_dirname}
+
+echo "compiling C++ to LLVM IR..." && \
 $clang --target=wasm32 -emit-llvm -c -S ${src_filename} -o ${ll_filename} && \
-$opt ${ll_filename} -load=$enzyme -enzyme -S -o ${ll_diff_filename} && \
+echo "differentiating LLVM IR..." && \
+$opt ${ll_filename} -load=$ENZYME -enzyme -S -o ${ll_diff_filename} && \
+echo "optimizing differentiated LLVM IR..." && \
 $opt ${ll_diff_filename} -S -o ${ll_diff_opt_filename} && \
+echo "compiling LLVM IR to WASM..." && \
 $llc -march=wasm32 -filetype=obj -o ${o_filename} ${ll_diff_opt_filename} && \
 $ld --no-entry -allow-undefined --export-all -o ${wasm_filename} ${o_filename} && \
 echo "saved to ${wasm_filename}"
