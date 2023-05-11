cae219fa29b8fd76a4ac24d489adc80e85ac83ed
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-10 20:07:45 -0500

    Test neural controller (#30)

---
+++ test/neural/generateTrajectory.js
@@ -0,0 +1,48 @@
+const algovivo = require("algovivo");
+const fsp = require("fs/promises");
+const NeuralPolicy = require("./NeuralPolicy");
+const utils = require("../utils");
+
+async function main() {
+  const system = new algovivo.System({
+    wasmInstance: await utils.loadWasm()
+  });
+  const meshDataPromise = fsp.readFile(`${__dirname}/data/mesh.json`);
+  const policyDataPromise = fsp.readFile(`${__dirname}/data/policy.json`);
+  const [meshData, policyData] = (await Promise.all([
+    meshDataPromise, policyDataPromise
+  ])).map(r => JSON.parse(r));
+  system.set(meshData);
+  const policy = new NeuralPolicy({
+    system: system,
+    stochastic: false,
+    active: true
+  });
+  policy.loadData(policyData);
+
+  const outputDirname = `${__dirname}/data/trajectory`;
+
+  await utils.cleandir(outputDirname);
+
+  const writePromises = [];
+  for (let i = 0; i < 100; i++) {
+    const itemData = {
+      x0: system.x0.toArray(),
+      v0: system.v0.toArray()
+    };
+
+    policy.step();
+    system.step();
+
+    itemData.x1 = system.x0.toArray();
+    itemData.v1 = system.v0.toArray();
+
+    const filename = `${outputDirname}/${i}.json`;
+    const p = fsp.writeFile(filename, JSON.stringify(itemData));
+    writePromises.push(p);
+  }
+
+  await Promise.all(writePromises);
+}
+
+main();
