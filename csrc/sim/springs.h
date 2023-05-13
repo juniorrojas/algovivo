3b44f30632b39be12ffdbc6d50f93b22d32f5bf2
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-13 15:43:37 -0500

    const auto

--- csrc/sim/springs.h
+++ csrc/sim/springs.h
@@ -2,22 +2,22 @@
 
 extern "C"
 void l0_of_x(int num_vertices, const float* x, int num_springs, const int* indices, float* l0) {
-  const int space_dim = 2;
+  const auto space_dim = 2;
 
   for (int i = 0; i < num_springs; i++) {
-    const int offset = 2 * i;
-    int i1 = indices[offset    ];
-    int i2 = indices[offset + 1];
+    const auto offset = 2 * i;
+    const auto i1 = indices[offset    ];
+    const auto i2 = indices[offset + 1];
     
-    auto p1x = x[i1 * space_dim    ];
-    auto p1y = x[i1 * space_dim + 1];
+    const auto p1x = x[i1 * space_dim    ];
+    const auto p1y = x[i1 * space_dim + 1];
 
-    auto p2x = x[i2 * space_dim    ];
-    auto p2y = x[i2 * space_dim + 1];
+    const auto p2x = x[i2 * space_dim    ];
+    const auto p2y = x[i2 * space_dim + 1];
 
-    auto dx = p1x - p2x;
-    auto dy = p1y - p2y;
-    auto q = dx * dx + dy * dy;
+    const auto dx = p1x - p2x;
+    const auto dy = p1y - p2y;
+    const auto q = dx * dx + dy * dy;
     l0[i] = __builtin_sqrt(q);
   }
 }
