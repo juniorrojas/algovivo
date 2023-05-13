8b7e8794086c1796848d0abb3a2eeb6ecf785ff1
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-13 00:17:40 -0500

    Move IconButton to separate file

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,47 +1,11 @@
 import algovivo from "../../build/algovivo.module.min.js";
 import NeuralPolicy from "./NeuralPolicy.js";
+import IconButton from "./IconButton.js";
 
 async function loadWasm() {
-  const wasm = await WebAssembly.instantiateStreaming(
-    await fetch("algovivo.wasm")
-  );
+  const response = await fetch("algovivo.wasm");
+  const wasm = await WebAssembly.instantiateStreaming(response);
   return wasm.instance;
-}
-
-class IconButton {
-  constructor(args = {}) {
-    const div = this.domElement = document.createElement("div");
-    div.style.userSelect = "none";
-    div.style.webkitTapHighlightColor = "transparent";
-    div.style.padding = "12px";
-    div.style.cursor = "pointer";
-    div.style.borderRadius = "50%";
-    div.style.width = "78px";
-    div.style.height = "78px";
-    div.style.minHeight = div.style.height;
-    div.style.margin = "4px";
-    div.style.display = "flex";
-    div.style.alignItems = "center";
-    div.style.justifyContent = "center";
-    div.style.boxShadow = "0 0 8px rgba(0, 0, 0, 0.2)";
-
-    const icon = new Image();
-    icon.src = args.src;
-    const iconSize = 40
-    icon.style.width = `${iconSize}px`;
-    icon.style.height = `${iconSize}px`;
-    div.appendChild(icon);
-
-    this.setInactiveStyle();
-  }
-
-  setActiveStyle() {
-    this.domElement.style.backgroundColor = "black";
-  }
-
-  setInactiveStyle() {
-    this.domElement.style.backgroundColor = "rgba(1, 1, 1, 0.2)";
-  }
 }
 
 const headerBackgroundColor = "#000000";
