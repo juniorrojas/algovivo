1879c4a8499baf93abaa8a115773c5d437a47cec
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-13 21:47:09 -0500

    Add failing test

--- test/springs.test.js
+++ test/springs.test.js
@@ -23,3 +23,28 @@ test("set x and springs", async () => {
   const expectedL0 = [1.4142135381698608, 1.4142135381698608, 2];
   expect(system.l0.toArray()).toBeCloseToArray(expectedL0);
 });
+
+test("set springs", async () => {
+  const wasmInstance = await utils.loadWasm();
+  const system = new algovivo.System({ wasmInstance });
+  system.set({
+    x: [
+      [0, 0],
+      [2, 0],
+      [1, 1],
+      [3, 7],
+      [5, 7]
+    ]
+  });
+  expect(system.numSprings()).toBe(0);
+  system.setSprings({
+    indices: [
+      [0, 2],
+      [1, 2],
+      [3, 4]
+    ]
+  });
+  expect(system.numSprings()).toBe(3);
+  const expectedL0 = [1.4142135381698608, 1.4142135381698608, 2];
+  expect(system.l0.toArray()).toBeCloseToArray(expectedL0);
+});
