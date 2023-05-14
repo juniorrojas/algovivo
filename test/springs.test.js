fe5e935c213e49181a8202630083b55313c95a4b
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-14 00:13:34 -0500

    Implement System.setSprings (#42)

--- test/springs.test.js
+++ test/springs.test.js
@@ -20,6 +20,43 @@ test("set x and springs", async () => {
       [3, 4]
     ]
   });
+  expect(system.numSprings()).toBe(3);
   const expectedL0 = [1.4142135381698608, 1.4142135381698608, 2];
   expect(system.l0.toArray()).toBeCloseToArray(expectedL0);
+});
+
+test("set springs", async () => {
+  const wasmInstance = await utils.loadWasm();
+  const system = new algovivo.System({ wasmInstance });
+  system.set({
+    x: [
+      [0, 0],
+      [2, 0],
+      [1, 1],
+      [3, 7],
+      [5, 7]
+    ]
+  });
+  expect(system.numSprings()).toBe(0);
+  
+  system.setSprings({
+    indices: [
+      [0, 2],
+      [1, 2],
+      [3, 4]
+    ]
+  });
+  expect(system.numSprings()).toBe(3);
+  const expectedL0 = [1.4142135381698608, 1.4142135381698608, 2];
+  expect(system.l0.toArray()).toBeCloseToArray(expectedL0);
+
+  system.setSprings({
+    indices: [
+      [0, 2],
+      [3, 4]
+    ],
+    l0: [10, 15]
+  });
+  expect(system.numSprings()).toBe(2);
+  expect(system.l0.toArray()).toBeCloseToArray([10, 15]);
 });
