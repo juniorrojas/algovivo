69d79612c919e095a06b2d84fd6a0bd847eaeb51
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-16 15:36:10 -0500

    const

--- csrc/sim/arr.h
+++ csrc/sim/arr.h
@@ -1,13 +1,13 @@
 #pragma once
 
 extern "C" void
-copy_(int n, float* src, float* dst) {
+copy_(int n, const float* src, float* dst) {
   for (int i = 0; i < n; i++) dst[i] = src[i];
 }
 
 extern "C" void
 // dst = a + b * c
-addmuls_(int n, float* a, float* b, float c, float* dst) {
+addmuls_(int n, const float* a, const float* b, const float c, float* dst) {
   for (int i = 0; i < n; i++) dst[i] = a[i] + b[i] * c;
 }
 
