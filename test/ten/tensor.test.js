835b6a1fc9f80441cc5a41725aa9a653c89dc326
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-17 14:00:51 -0500

    Refactor tensor ops and add tests (#49)

---
+++ test/ten/tensor.test.js
@@ -0,0 +1,38 @@
+const { mmgrten } = require("algovivo");
+const utils = require("../utils");
+
+test("tensor", async () => {
+  const ten = await mmgrten.engine({
+    wasmInstance: await utils.loadWasm()
+  });
+  const a = ten.tensor([1, 2, 3]);
+  expect(a.shape.toArray()).toEqual([3]);
+  expect(a.stride.toArray()).toEqual([1]);
+  expect(a.order).toEqual(1);
+  expect(a.numel).toEqual(3);
+
+  expect(Array.from(a.typedArray())).toEqual([1, 2, 3]);
+
+  expect(a.get([0])).toBe(1);
+  a.set([0], 10);
+  expect(a.get([0])).toBe(10);
+  
+  expect(a.flattenIdx([1])).toBe(1);
+  expect(a.get([1])).toBe(2);
+  expect(a.flattenIdx([2])).toBe(2);
+  expect(a.get([2])).toBe(3);
+});
+
+test("matrix set", async () => {
+  const ten = await mmgrten.engine({
+    wasmInstance: await utils.loadWasm()
+  });
+
+  const a = ten.zeros([2, 3]);
+  a.set([1, 2], 10);
+  a.set([0, 1], 30);
+  expect(a.toArray()).toEqual([
+    [0, 30, 0],
+    [0, 0, 10]
+  ]);
+});
