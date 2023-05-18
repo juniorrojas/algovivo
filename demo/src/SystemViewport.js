1c80e04d3bcc3c381640add6d8f12c088699e28f
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-18 17:07:44 -0500

    Update media query

--- demo/src/SystemViewport.js
+++ demo/src/SystemViewport.js
@@ -7,7 +7,7 @@ export default class SystemViewport extends algovivo.SystemViewport {
     this.domElement.style.border = "2px solid #c9c9c9";
     this.domElement.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
     
-    const mq = window.matchMedia("(max-width: 410px)");
+    const mq = window.matchMedia("(max-width: 425px)");
     const updateMq = () => {
       if (mq.matches) {
         this.setSize({ width: 350, height: 350});
