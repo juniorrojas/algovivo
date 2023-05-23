8fe04f9caec4d3398959632a57e88e13a26c0ee3
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-22 19:28:33 -0500

    Fix typo

--- test/neural/neural.test.js
+++ test/neural/neural.test.js
@@ -44,7 +44,7 @@ test("neural policy", async () => {
   let expectedNumReservedBytes = null;
   const mgr = system.memoryManager;
 
-  // TODO infer numbe of steps in trajectoryDataDirname
+  // TODO infer number of steps in trajectoryDataDirname
   for (let i = 0; i < 100; i++) {
     const data = JSON.parse(fs.readFileSync(`${trajectoryDataDirname}/${i}.json`));
 
