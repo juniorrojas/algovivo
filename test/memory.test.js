a5c0568935aab07197d5dddb2a486c8c36932e74
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-24 04:17:26 -0500

    Update System.dispose to allow for data reset (#58)

--- test/memory.test.js
+++ test/memory.test.js
@@ -11,34 +11,48 @@ test("memory", async () => {
   expect(system.numSprings()).toBe(0);
   expect(system.numTriangles()).toBe(0);
 
-  const mesh = {};
-  mesh.triangles = [];
-  mesh.x = [
-    [0, 0],
-    [1, 0],
-    [1, 1]
-  ];
-  mesh.triangles = [
-    [0, 1, 2]
-  ];
-  mesh.springs = [
-    [0, 1],
-    [2, 1]
-  ];
+  const meshData = {
+    x: [
+      [0, 0],
+      [1, 0],
+      [1, 1]
+    ],
+    springs: [
+      [0, 1],
+      [2, 1]
+    ],
+    triangles: [
+      [0, 1, 2]
+    ]
+  };
 
   expect(memoryManager.numReservedBytes()).toBe(0);
-  system.set(mesh);
+  system.set(meshData);
   expect(memoryManager.numReservedBytes()).not.toBe(0);
   const reservedBytes = memoryManager.numReservedBytes();
   expect(system.numVertices()).toBe(3);
   expect(system.numTriangles()).toBe(1);
   expect(system.numSprings()).toBe(2);
+  
   // updating the mesh again with the same data
   // should not allocate any new memory
-  system.set(mesh);
+  system.set(meshData);
   expect(memoryManager.numReservedBytes()).toBe(reservedBytes);
+  expect(system.numVertices()).toBe(3);
+  expect(system.numTriangles()).toBe(1);
+  expect(system.numSprings()).toBe(2);
 
   // free memory
   system.dispose();
   expect(memoryManager.numReservedBytes()).toBe(0);
+  expect(system.numVertices()).toBe(0);
+  expect(system.numTriangles()).toBe(0);
+  expect(system.numSprings()).toBe(0);
+  
+  // reset data
+  system.set(meshData);
+  expect(memoryManager.numReservedBytes()).toBe(reservedBytes);
+  expect(system.numVertices()).toBe(3);
+  expect(system.numTriangles()).toBe(1);
+  expect(system.numSprings()).toBe(2);
 });
