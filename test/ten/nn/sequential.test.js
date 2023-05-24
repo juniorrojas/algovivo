50c3c79ca24937705b33775ee4d84ebb84fdbef9
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-23 23:51:22 -0500

    Update whitespace

--- test/ten/nn/sequential.test.js
+++ test/ten/nn/sequential.test.js
@@ -7,6 +7,7 @@ test("sequential", async () => {
   });
   const nn = ten.nn;
   const mgr = ten.mgr;
+
   const bytes0 = ten.mgr.numReservedBytes();
 
   const inputSize = 3;
@@ -24,10 +25,10 @@ test("sequential", async () => {
     4,
     3
   ]);
-
+  
   const output = model.forward(input);
+  expect(output.toArray().length).toBe(2);
 
-  expect(output.toArray().length).toBe(2);  
   expect(mgr.numReservedBytes()).not.toBe(bytes0);
   model.dispose();
   input.dispose();
