3491326c6c62dc459b638dd97503202e15fec443
Junior Rojas <jrojasdavalos@gmail.com>
2023-05-25 14:38:23 -0500

    bin

--- experimental/bin/computeRestShape.js
+++ experimental/bin/computeRestShape.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import algovivo from "algovivo";
 import fsp from "fs/promises";
 import { fileURLToPath } from "url";
@@ -8,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 async function loadWasm() {
-  const wasmFilename = __dirname + "/node_modules/algovivo/build/algovivo.wasm";
+  const wasmFilename = __dirname + "/../node_modules/algovivo/build/algovivo.wasm";
   const wasm = await WebAssembly.compile(await fsp.readFile(wasmFilename));
   const wasmInstance = await WebAssembly.instantiate(wasm);
   return wasmInstance;
@@ -36,7 +37,7 @@ async function main() {
   console.log(`triangles: ${system.numTriangles()}`);
   meshData.l0 = system.l0.toArray();
   meshData.rsi = system.rsi.toArray();
-  const outputFilename = __dirname + "/mesh.out.json";
+  const outputFilename = "mesh.out.json";
   await fsp.writeFile(outputFilename, JSON.stringify(meshData));
   console.log(`saved to ${outputFilename}`);
 }
