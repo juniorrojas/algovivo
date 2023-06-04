b7bc2cd763919f8c0f89c4587699d0a5ea6d956b
Junior Rojas <jrojasdavalos@gmail.com>
2023-06-04 07:16:33 -0500

    Refactor csrc (#66)

--- csrc/algovivo/springs.h
+++ csrc/algovivo/springs.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "vec2.h"
-#include "vertices.h"
 
 namespace algovivo {
 
--- csrc/algovivo/vec2.h
+++ csrc/algovivo/vec2.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define vec2_get(name, x, i) \
-  const auto (name##x) = x[2 * i    ]; \
-  const auto (name##y) = x[2 * i + 1];
+#define vec2_get(name, arr, i) \
+  const auto (name##x) = arr[2 * i    ]; \
+  const auto (name##y) = arr[2 * i + 1];
 
 #define vec2_sub(r, a, b) \
   const auto (r##x) = (a##x) - (b##x); \
