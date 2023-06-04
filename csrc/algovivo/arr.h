5128068cac24952f27e17f4d5f0f626075d86639
Junior Rojas <jrojasdavalos@gmail.com>
2023-06-04 01:10:24 -0500

    Refactor csrc (#64)

---
+++ csrc/algovivo/arr.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace algovivo {
+
+void copy_(int n, const float* src, float* dst) {
+  for (int i = 0; i < n; i++) dst[i] = src[i];
+}
+
+// dst = a + b * c
+void addmuls_(int n, const float* a, const float* b, const float c, float* dst) {
+  for (int i = 0; i < n; i++) dst[i] = a[i] + b[i] * c;
+}
+
+void scale_(int n, float* a, float c) {
+  for (int i = 0; i < n; i++) a[i] *= c;
+}
+
+}
