2e79cce24898090f293adae03e541ff5da6ea1ea
Junior Rojas <jrojasdavalos@gmail.com>
2023-07-14 21:57:20 -0700

    Implement getters

--- test/mm2d/AABB.test.js
+++ test/mm2d/AABB.test.js
@@ -5,11 +5,13 @@ test("basic", () => {
     x0: 20,
     y0: 15,
     width: 10,
-    height: 30,
+    height: 40,
   });
-  expect(aabb.x0()).toEqual(20);
-  expect(aabb.x1()).toEqual(20 + 10);
-  expect(aabb.y0()).toEqual(15);
-  expect(aabb.y1()).toEqual(15 + 30);
-  expect(aabb.center()).toEqual([25, 30]);
+  expect(aabb.x0).toEqual(20);
+  expect(aabb.x1).toEqual(30);
+  expect(aabb.width).toEqual(10);
+  expect(aabb.height).toEqual(40);
+  expect(aabb.y0).toEqual(15);
+  expect(aabb.y1).toEqual(55);
+  expect(aabb.center).toEqual([25, 35]);
 });
