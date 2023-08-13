818d05351a6f1952273c660b7e28b4948f3a6b21
Junior Rojas <jrojasdavalos@gmail.com>
2023-08-13 00:37:01 -0700

    Refactor test

--- test/ten/tensor.test.js
+++ test/ten/tensor.test.js
@@ -38,23 +38,3 @@ test("matrix set", async () => {
     [0, 0, 10]
   ]);
 });
-
-test("fill", async () => {
-  const ten = await mmgrten.engine({
-    wasmInstance: await utils.loadWasm()
-  });
-
-  const a = ten.zeros([2, 3]);
-
-  a.fill_(2);
-  expect(a.toArray()).toEqual([
-    [2, 2, 2],
-    [2, 2, 2]
-  ]);
-
-  a.zero_();
-  expect(a.toArray()).toEqual([
-    [0, 0, 0],
-    [0, 0, 0]
-  ]);
-})
