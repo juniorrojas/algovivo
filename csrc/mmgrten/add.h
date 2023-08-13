1606b30f3a685ba488ecb5054d112f8051cd51ba
Junior Rojas <jrojasdavalos@gmail.com>
2023-08-13 01:11:11 -0700

    const

--- csrc/mmgrten/add.h
+++ csrc/mmgrten/add.h
@@ -5,8 +5,8 @@ namespace mmgrten {
 extern "C"
 void add(
   int a_numel,
-  float* a_data,
-  float* b_data,
+  const float* a_data,
+  const float* b_data,
   float* c_data
 ) {
   for (int i = 0; i < a_numel; i++) {
