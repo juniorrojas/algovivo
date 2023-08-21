94e46fb9c5d23c1cc9ccb758b49e7a0fc5dc87be
Junior Rojas <jrojasdavalos@gmail.com>
2023-08-20 17:05:29 -0700

    Update demo

--- demo/src/SystemViewport.js
+++ demo/src/SystemViewport.js
@@ -11,13 +11,13 @@ export default class SystemViewport extends algovivo.SystemViewport {
     this.domElement.style.border = "2px solid #c9c9c9";
     this.domElement.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
     
-    const mq = window.matchMedia("(max-width: 425px)");
+    const mq = window.matchMedia("(max-width: 410px)");
     const updateMq = () => {
       if (mq.matches) {
-        this.setSize({ width: 350, height: 350});
+        this.setSize({ width: 300, height: 350});
       } else {
         this.setSize({ width: 400, height: 400});
-      }this
+      }
     }
     mq.addEventListener("change", (event) => {
       updateMq();
