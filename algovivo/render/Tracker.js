567da5f85b31aecf63d4f7ad932b95097c774635
Junior Rojas <jrojasdavalos@gmail.com>
2023-09-29 23:38:29 -0700

    Update tracker, compute viewport limits

--- algovivo/render/Tracker.js
+++ algovivo/render/Tracker.js
@@ -29,14 +29,35 @@ class Tracker {
     const recenterThreshold = 3;
     const cx = this.currentCenterX;
     const tx = Math.floor(cx / recenterThreshold) * recenterThreshold;
-    grid.mesh.setCustomAttribute(
-      "translation",
-      [tx, 0]
-    );
-    floor.mesh.setCustomAttribute(
-      "translation",
-      [tx, 0]
-    );
+    const topLeft = viewport.camera.domToWorldSpace([0, 0]);
+    const topRight = viewport.camera.domToWorldSpace([viewport.renderer.width, 0]);
+    const bottomRight = viewport.camera.domToWorldSpace([viewport.renderer.width, viewport.renderer.height]);
+    const bottomLeft = viewport.camera.domToWorldSpace([0, viewport.renderer.height]);
+    
+    const [_x0, _y0] = bottomLeft;
+    const x0 = Math.floor(_x0);
+    let y0 = Math.floor(_y0);
+    if (y0 < 0) {
+      y0 = 0;
+    }
+    const [x1, y1] = topRight;
+
+    const width = x1 - x0;
+    const height = y1 - y0;
+    const rows = height;
+    const cols = width;
+
+    grid.set({
+      x0: x0,
+      y0: y0,
+      rows: rows,
+      cols: cols
+    });
+
+    floor.mesh.x = [
+      [x0, 0],
+      [x1, 0]
+    ];
 
     const center = [this.currentCenterX, 1];
     camera.center({
