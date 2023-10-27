13f9187193724395508380bb7f0744e2300e9d62
Junior Rojas <jrojasdavalos@gmail.com>
2023-10-26 21:33:25 -0700

    Update viewport hit test to work with zero vertices (#107)

--- algovivo/render/SystemViewport.js
+++ algovivo/render/SystemViewport.js
@@ -474,6 +474,7 @@ class SystemViewport {
 
   hitTestVertex(p, hitTestRadius = 0.31) {
     const numVertices = this.system.numVertices();
+    if (numVertices == 0) return null;
     const xF32 = this.system.x0.slot.f32();
     let closestVertex = null;
     let closestQuadrance = Infinity;
