72a5741b351f220dd01b1cd6d662b53e22b6b441
Junior Rojas <jrojasdavalos@gmail.com>
2023-12-15 20:02:28 -0800

    Refactor

--- csrc/algovivo/backward_euler.h
+++ csrc/algovivo/backward_euler.h
@@ -14,39 +14,39 @@ namespace algovivo {
 template <typename T>
 void backward_euler_update_pos(
   T system,
-  float* x,
-  float* x_grad, float* x_tmp
+  float* pos,
+  float* pos_grad, float* pos_tmp
 ) {
   const auto space_dim = 2;
   const auto num_vertices = system.num_vertices;
-  const auto x0 = system.pos0;
-  const auto v = system.vel0;
+  const auto pos0 = system.pos0;
+  const auto vel = system.vel0;
   const auto fixed_vertex_id = system.fixed_vertex_id;
   const auto h = system.h;
 
   for (int i = 0; i < num_vertices; i++) {
     const auto offset = i * space_dim;
-    x[offset    ] = x0[offset    ] + h * v[offset    ];
-    x[offset + 1] = x0[offset + 1] + h * v[offset + 1];
+    pos[offset    ] = pos0[offset    ] + h * vel[offset    ];
+    pos[offset + 1] = pos0[offset + 1] + h * vel[offset + 1];
   }
 
   const auto max_optim_iters = 100;
   for (int i = 0; i < max_optim_iters; i++) {
-    zero_(num_vertices * space_dim, x_grad);
-    system.backward(x, x_grad);
+    zero_(num_vertices * space_dim, pos_grad);
+    system.backward(pos, pos_grad);
 
     if (fixed_vertex_id > -1) {
       const auto offset = fixed_vertex_id * space_dim;
-      x_grad[offset    ] = 0.0;
-      x_grad[offset + 1] = 0.0;
+      pos_grad[offset    ] = 0.0;
+      pos_grad[offset + 1] = 0.0;
     }
     
     float grad_max_q = 0.0;
     float grad_q_tol = 0.5 * 1e-5;
     for (int k = 0; k < num_vertices; k++) {
       int offset = k * space_dim;
-      float px = x_grad[offset    ];
-      float py = x_grad[offset + 1];
+      float px = pos_grad[offset    ];
+      float py = pos_grad[offset + 1];
       float q = px * px + py * py;
       if (q > grad_max_q) grad_max_q = q;
     }
@@ -56,11 +56,11 @@ void backward_euler_update_pos(
     const auto max_line_search_iters = 20;
     float backtracking_scale = 0.3;
 
-    const auto loss0 = system.forward(x);
+    const auto loss0 = system.forward(pos);
 
     for (int i = 0; i < max_line_search_iters; i++) {
-      addmuls_(num_vertices * space_dim, x, x_grad, -step_size, x_tmp);
-      const auto loss1 = system.forward(x_tmp);
+      addmuls_(num_vertices * space_dim, pos, pos_grad, -step_size, pos_tmp);
+      const auto loss1 = system.forward(pos_tmp);
       if (loss1 < loss0) {
         break;
       } else {
@@ -68,7 +68,7 @@ void backward_euler_update_pos(
       }
     }
     
-    addmuls_(num_vertices * space_dim, x, x_grad, -step_size, x);
+    addmuls_(num_vertices * space_dim, pos, pos_grad, -step_size, pos);
   }
 }
 
