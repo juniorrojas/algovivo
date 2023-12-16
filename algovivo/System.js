37baffdd0053067267e471fe421222a57c043559
Junior Rojas <jrojasdavalos@gmail.com>
2023-12-15 19:45:43 -0800

    Update function call

--- algovivo/System.js
+++ algovivo/System.js
@@ -104,7 +104,7 @@ class System {
       this.l0 = l0;
 
       if (args.l0 == null) {
-        this.wasmInstance.exports.l0_of_x(
+        this.wasmInstance.exports.l0_of_pos(
           this.numVertices,
           this.x0.ptr,
           numMuscles,
