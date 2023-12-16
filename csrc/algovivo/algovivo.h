05b1b4469443115152fc2a3ae60278b77067059c
Junior Rojas <jrojasdavalos@gmail.com>
2023-12-15 19:17:23 -0800

    Update #includes

--- csrc/algovivo/algovivo.h
+++ csrc/algovivo/algovivo.h
@@ -1,5 +1,11 @@
 #pragma once
 
+// energy functions
 #include "muscles.h"
 #include "triangles.h"
+#include "gravity.h"
+#include "collision.h"
+#include "friction.h"
+
+// numerical integration
 #include "backward_euler.h"
