ab9b2d95ac91b963e331b2cb5ea649b51d3e456e
Junior Rojas <jrojasdavalos@gmail.com>
2023-12-15 19:19:25 -0800

    Refactor csrc (#114)

--- csrc/algovivo/algovivo.h
+++ csrc/algovivo/algovivo.h
@@ -1,5 +1,11 @@
 #pragma once
 
-#include "springs.h"
+// energy functions
+#include "muscles.h"
 #include "triangles.h"
+#include "gravity.h"
+#include "collision.h"
+#include "friction.h"
+
+// numerical integration
 #include "backward_euler.h"
