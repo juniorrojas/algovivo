ec27770ba350683a2d53d92cf4fa218a593eb7b7
Junior Rojas <jrojasdavalos@gmail.com>
2024-06-27 20:34:52 -0700

    Implement codegen utils (#168)

---
+++ utils/codegen/system.template.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "vec2.h"
+#include "dynamics/inertia.h"
+#include "potential/friction.h"
+#include "potential/collision.h"
+#include "potential/gravity.h"
+
+namespace algovivo {
+
+static float backward_euler_loss(
+  // {{backward_euler_loss_args}}
+) {
+  // {{backward_euler_loss_body}}
+}
+
+static void backward_euler_loss_grad(
+  // {{backward_euler_loss_grad_args}}
+) {
+  // {{backward_euler_loss_grad_body}}
+}
+
+struct System {
+  // {{system_attrs}}
+
+  float forward(float* pos) {
+    return backward_euler_loss(
+      // {{backward_euler_loss_args_call}}
+    );
+  }
+
+  void backward(float* pos, float* pos_grad) {
+    backward_euler_loss_grad(
+      // {{backward_euler_loss_grad_args_call}}
+    );
+  }
+};
+
+extern "C"
+void backward_euler_update(
+  // {{update_args}}
+) {
+  algovivo::System system;
+
+  // {{system_set}}
+
+  algovivo::backward_euler_update(
+    system,
+    pos1, vel1,
+    pos_grad, pos_tmp
+  );
+}
+
+}
