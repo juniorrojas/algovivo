e750e3eb180841473e352ad3df95247b7491c9e1
Junior Rojas <jrojasdavalos@gmail.com>
2024-07-18 00:03:22 -0700

    Refactor

--- demo/src/Description.js
+++ demo/src/Description.js
@@ -11,7 +11,9 @@ export default class Description {
     this.domElement.style.display = "flex";
     this.domElement.style.flexDirection = "column";
 
-    let s = new Section(
+    let s;
+
+    s = this.addSection(
       "energy-based models for virtual creatures",
 `<p>
 The concept of energy minimization is useful to understand physical systems as goal-directed systems.
@@ -21,9 +23,8 @@ This more general notion of energy is useful to model inertia, friction, actuati
 </p>`
     );
     s.setStyle1();
-    this.domElement.appendChild(s.domElement);
 
-    s = new Section(
+    s = this.addSection(
       "no forces, just energy functions",
 `<p>
 Much like the loss function encapsulates the objective of a neural network in a single scalar value, potential energy functions offer a scalar representation of the objective of a physical system.
@@ -31,9 +32,8 @@ In practice, just as we typically do not compute neural network gradients by han
 The force can be straightforwardly derived as the negative gradient of the potential energy.
 For general energy functions that are not plain potential energy functions, the negative gradient cannot be directly interpreted as a force, but it can still be used for gradient-based optimization and is useful for implicit numerical integration methods.
 </p>`);
-    this.domElement.appendChild(s.domElement);
 
-    s = new Section(
+    s = this.addSection(
       "six energy functions",
 `<p>
 This implementation defines six energy functions. Some are plain potential energy functions, that is, functions of vertex positions <span class="code">E(pos)</span>. Actuation mechanisms can be modeled with an action-dependent energy function <span class="code">E(pos, a)</span>. Other energy functions may also depend on the previous state, given by vertex positions and velocities <span class="code">(pos0, vel0)</span>.
@@ -47,8 +47,13 @@ This implementation defines six energy functions. Some are plain potential energ
 <li>friction <span class="code">E(pos, pos0)</span></li>
 <li>inertia <span class="code">E(pos, pos0, vel0)</span></li>
 </ul>`
-);
+    );
     s.setStyle1();
-    this.domElement.appendChild(s.domElement);
+  }
+
+  addSection(title, content) {
+    const section = new Section(title, content);
+    this.domElement.appendChild(section.domElement);
+    return section;
   }
 }
