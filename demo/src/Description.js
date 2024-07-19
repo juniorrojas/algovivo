728cf651a6a747da15dc63f9fd7ac4a9ef1a96c7
Junior Rojas <jrojasdavalos@gmail.com>
2024-07-18 20:00:47 -0700

    Update demo (#182)

--- demo/src/Description.js
+++ demo/src/Description.js
@@ -16,10 +16,10 @@ export default class Description {
     s = this.addSection(
       "energy-based models for virtual creatures",
 `<p>
-The concept of energy minimization is useful to understand physical systems as goal-directed systems.
+Potential energy minimization is one of the <a href="https://youtu.be/c8iFtaltX-s?si=MDUb20nFhumn1Q3n&t=3553">most basic forms of goal-directed behavior</a>.
 A deformable object that tends to recover its rest shape can be understood as a system whose goal is to minimize its elastic potential energy.
-More generally, energy can refer to any scalar-valued function that measures compatibility between variables as a means of implicitly capturing their dependencies through energy minimization.
-This more general notion of energy is useful to model inertia, friction, actuation mechanisms and, potentially, many other goal-directed behaviors that may or may not be conventionally considered &ldquo;just physics&rdquo;.
+More generally, &ldquo;energy&rdquo; in the context of <a href="https://youtu.be/MiqLoAZFRSE?t=2177">energy-based models</a> refers to any implicit function that captures dependencies between variables.
+This more general notion of energy is consistent with potential energy, but is also useful to model inertia, friction, actuation mechanisms and many other goal-directed behaviors that may or may not be conventionally considered <a href="https://youtu.be/lIHUWOv4nkE?t=629">&ldquo;just physics&rdquo;</a>.
 </p>`
     );
     s.setStyle1();
@@ -28,7 +28,7 @@ This more general notion of energy is useful to model inertia, friction, actuati
       "no forces, just energy functions",
 `<p>
 Much like the loss function encapsulates the objective of a neural network in a single scalar value, potential energy functions offer a scalar representation of the objective of a physical system.
-In practice, just as we typically do not compute neural network gradients by hand, we can also avoid computing forces by hand.
+In practice, just as we typically do not compute neural network gradients by hand, we can also avoid computing forces by hand if we use automatic differentiation.
 The force can be straightforwardly derived as the negative gradient of the potential energy.
 For general energy functions that are not plain potential energy functions, the negative gradient cannot be directly interpreted as a force, but it can still be used for gradient-based optimization and is useful for implicit numerical integration methods.
 </p>`);
@@ -36,7 +36,7 @@ For general energy functions that are not plain potential energy functions, the 
     s = this.addSection(
       "six energy functions",
 `<p>
-This implementation defines six energy functions. Some are plain potential energy functions, that is, functions of vertex positions <span class="code">E(pos)</span>. Actuation mechanisms can be modeled with an action-dependent energy function <span class="code">E(pos, a)</span>. Other energy functions may also depend on the previous state, given by vertex positions and velocities <span class="code">(pos0, vel0)</span>.
+This implementation defines six energy functions. Some are plain potential energy functions, that is, functions of vertex positions <span class="code">E(pos)</span>. Actuation mechanisms are modeled with an action-dependent energy function <span class="code">E(pos, a)</span>. Other energy functions may also depend on the previous state, given by vertex positions and velocities <span class="code">(pos0, vel0)</span>.
 </p>
 
 <ul>
