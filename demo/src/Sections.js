95dffe3a27e75a21555d6568d32c1f36daa3e5e3
Junior Rojas <jrojasdavalos@gmail.com>
2024-07-20 09:05:47 -0700

    Fix typo

--- demo/src/Sections.js
+++ demo/src/Sections.js
@@ -78,7 +78,7 @@ It is actually possible to implement <a class="a2" href="https://github.com/juni
 When we say energy minimization, we mean something more akin to the least action principle, except that &ldquo;action&rdquo; has a very specific definition in Lagrangian mechanics, and we are not using that definition here.
 We also want to reserve the word &ldquo;action&rdquo; to refer to actuation mechanisms such as muscle control, which are modeled as action-dependent energy functions.
 Confusing terminology, I know.
-Whatever variable or function name you prefer to use in your code, the important thing it to have a <a href="https://github.com/juniorrojas/algovivo" class="a2">working implementation</a> that goes beyond just words and mathematical formulas that may not be computable.
+Whatever variable or function name you prefer to use in your code, the important thing is to have a <a href="https://github.com/juniorrojas/algovivo" class="a2">working implementation</a> that goes beyond just words and mathematical formulas that may not be computable.
 </p>`
     );
   }
