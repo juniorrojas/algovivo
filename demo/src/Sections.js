a2b0d5c8671b8697d2996f498f2707a284d717c1
Junior Rojas <jrojasdavalos@gmail.com>
2024-07-22 01:00:56 +0200

    Update content

--- demo/src/Sections.js
+++ demo/src/Sections.js
@@ -17,7 +17,7 @@ export default class Sections {
       "energy-based models for virtual creatures",
 `<p>
 Potential energy minimization is one of the <a href="https://youtu.be/c8iFtaltX-s?si=MDUb20nFhumn1Q3n&t=3553">most basic forms of goal-directed behavior</a>.
-A deformable object that tends to recover its rest shape can be understood as a system whose goal is to minimize its elastic potential energy.
+A deformable object that tends to recover its original shape can be understood as a system whose goal is to minimize its elastic potential energy.
 More generally, &ldquo;energy&rdquo; in the context of <a href="https://youtu.be/MiqLoAZFRSE?t=2177">energy-based models</a> refers to any implicit function that captures dependencies between variables.
 This more general notion of energy is consistent with potential energy, and is also useful to model inertia, friction, actuation mechanisms and many other goal-directed behaviors that may or may not be conventionally considered <a href="https://youtu.be/lIHUWOv4nkE?t=629">&ldquo;just physics&rdquo;</a>.
 </p>`
@@ -82,8 +82,7 @@ Energy-based models can also easily describe dissipative systems.
 <a href="https://www.youtube.com/watch?v=7fRfxiyTKS0">Lagrangians and Hamiltonians cannot really describe dissipative systems</a>.
 When we say energy minimization, we mean something akin to the least action principle, except that &ldquo;action&rdquo; has a very specific definition in Lagrangian mechanics, and we are not using that definition here.
 We also want to reserve the word &ldquo;action&rdquo; to refer to actuation mechanisms such as muscle control, which are modeled as action-dependent energy functions.
-Confusing terminology, I know.
-Whatever variable or function names you prefer to use in your code, the important thing is to have a <a href="https://github.com/juniorrojas/algovivo">working implementation</a> that goes beyond just words and mathematical formulas that may not be computable.
+Whatever variable or function names you prefer to use in your code, the important thing is to have a <a href="https://github.com/juniorrojas/algovivo">working implementation</a> that goes beyond just words and mathematical formulas that may not even be computable.
 </p>`);
     s.setStyle1();
   }
