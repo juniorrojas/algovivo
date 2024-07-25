3b1896a4e50ea518b5b3ae7e465e3a960c2adc25
Junior Rojas <jrojasdavalos@gmail.com>
2024-07-25 11:00:01 +0200

    Update demo

--- demo/src/Sections.js
+++ demo/src/Sections.js
@@ -19,7 +19,7 @@ export default class Sections {
 <p>
 Potential energy minimization is one of the <a href="https://youtu.be/c8iFtaltX-s?si=MDUb20nFhumn1Q3n&t=3553">most basic forms of goal-directed behavior</a>.
 A deformable object that tends to recover its original shape can be understood as a system whose goal is to minimize its elastic potential energy.
-More generally, &ldquo;energy&rdquo; in the context of <a href="https://youtu.be/MiqLoAZFRSE?t=2177">energy-based models</a> refers to any implicit function that captures dependencies between variables.
+In the context of <a href="https://youtu.be/MiqLoAZFRSE?t=2177">energy-based models</a>, &ldquo;energy&rdquo; is used to define implicit functions that capture dependencies between variables.
 This more general notion of energy is consistent with potential energy, and is also useful to model inertia, friction, actuation mechanisms and many other goal-directed behaviors that may or may not be conventionally considered <a href="https://youtu.be/lIHUWOv4nkE?t=629">&ldquo;just physics&rdquo;</a>.
 </p>
 <a href="https://direct.mit.edu/isal/proceedings/isal2024/36/30/123447" style="padding-top: 25px; display: flex; flex-direction: column; align-items: center">
