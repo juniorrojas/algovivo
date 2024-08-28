bf633ad4345a4e7df7dbb546ce6de0a66fc8a853
Junior Rojas <jrojasdavalos@gmail.com>
2024-08-27 22:26:28 -0700

    Define ViewportMuscles

--- algovivo/render/SystemViewport.js
+++ algovivo/render/SystemViewport.js
@@ -2,6 +2,7 @@ const mm2d = require("./mm2d");
 const Tracker = require("./Tracker");
 const Floor = require("./Floor");
 const ViewportVertices = require("./ViewportVertices");
+const ViewportMuscles = require("./ViewportMuscles");
 
 function hashSimplex(vids) {
   vids.sort();
@@ -55,6 +56,9 @@ class SystemViewport {
     this.vertices = new ViewportVertices({
       system: this.system,
       renderVertexIds: args.renderVertexIds ?? false
+    });
+    this.muscles = new ViewportMuscles({
+      system: this.system
     });
 
     const renderer = new mm2d.Renderer({ headless });
@@ -149,77 +153,11 @@ class SystemViewport {
       ctx.closePath();
       ctx.fill();
     };
-    mesh.lineShader.renderLine = (args = {}) => {
-      const ctx = args.ctx;
-      const a = args.a;
-      const b = args.b;
-      const camera = args.camera;
-      const scale = camera.inferScale();
-
-      const lineIdToMuscleId = args.mesh.getCustomAttribute("lineIdToMuscleId");
-      const muscleId = lineIdToMuscleId[args.id];
-      if (muscleId == null) {
-        const borderWidth = 0.029;
-        ctx.beginPath();
-        ctx.lineJoin = "round";
-        ctx.lineCap = "round";
-        ctx.strokeStyle = borderColor;
-        ctx.lineWidth = borderWidth * scale;
-        ctx.moveTo(a[0], a[1]);
-        ctx.lineTo(b[0], b[1]);
-        ctx.closePath();
-        ctx.stroke();
-      } else {
-        const color0 = activeMuscleColor;
-        const color1 = inactiveMuscleColor;
-        
-        const width = 0.065;
-        const borderWidth = 0.017;
-        const lineCap = "butt";
-        const muscleIntensityAttributeName = "muscleIntensity";
-
-        ctx.beginPath();
-        ctx.lineCap = lineCap;
-        ctx.strokeStyle = borderColor;
-        ctx.lineWidth = (width + borderWidth * 2) * scale;
-        ctx.moveTo(a[0], a[1]);
-        ctx.lineTo(b[0], b[1]);
-        ctx.stroke();
-
-        ctx.beginPath();
-
-        const muscleIntensity = mesh.getCustomAttribute(muscleIntensityAttributeName);
-        if (muscleIntensity == null) {
-          throw new Error(`muscle intensity attribute (${muscleIntensityAttributeName}) not found, call setCustomAttribute("${muscleIntensityAttributeName}", value) before rendering.`);
-        }
-        if (!Array.isArray(muscleIntensity)) {
-          throw new Error(`muscle intensity attribute must be an array with values for each fiber, found ${typeof muscleIntensity}`);
-        }
-        
-        const t = muscleIntensity[muscleId];
-        
-        const cr0 = color0[0];
-        const cr1 = color1[0];
-
-        const cg0 = color0[1];
-        const cg1 = color1[1];
-
-        const cb0 = color0[2];
-        const cb1 = color1[2];
-
-        const cr = (1 - t) * cr0 + t * cr1;
-        const cg = (1 - t) * cg0 + t * cg1;
-        const cb = (1 - t) * cb0 + t * cb1;
-
-        ctx.strokeStyle = `rgb(${cr}, ${cg}, ${cb})`;
-        ctx.lineCap = lineCap;
-        ctx.lineWidth = width * scale;
-        ctx.moveTo(a[0], a[1]);
-        ctx.lineTo(b[0], b[1]);
-
-        ctx.stroke();
-      }
-    }
+    mesh.lineShader.renderLine = this.muscles.makeLineShaderFunction({
+      activeMuscleColor: activeMuscleColor,
+      inactiveMuscleColor: inactiveMuscleColor,
+      borderColor: borderColor
+    });
 
     const draggable = args.draggable ?? true;
     if (draggable) {
---
+++ algovivo/render/ViewportMuscles.js
@@ -0,0 +1,85 @@
+class ViewportMuscles {
+  constructor(args = {}) {
+    this.system = args.system;
+  }
+
+  makeLineShaderFunction(args = {}) {
+    const activeMuscleColor = args.activeMuscleColor ?? [255, 0, 0];
+    const inactiveMuscleColor = args.inactiveMuscleColor ?? [0, 0, 255];
+    const borderColor = args.borderColor ?? "black";
+    
+    return (args = {}) => {
+      const ctx = args.ctx;
+      const a = args.a;
+      const b = args.b;
+      const camera = args.camera;
+      const scale = camera.inferScale();
+
+      const lineIdToMuscleId = args.mesh.getCustomAttribute("lineIdToMuscleId");
+      const muscleId = lineIdToMuscleId[args.id];
+      if (muscleId == null) {
+        const borderWidth = 0.029;
+        ctx.beginPath();
+        ctx.lineJoin = "round";
+        ctx.lineCap = "round";
+        ctx.strokeStyle = borderColor;
+        ctx.lineWidth = borderWidth * scale;
+        ctx.moveTo(a[0], a[1]);
+        ctx.lineTo(b[0], b[1]);
+        ctx.closePath();
+        ctx.stroke();
+      } else {
+        const color0 = activeMuscleColor;
+        const color1 = inactiveMuscleColor;
+        
+        const width = 0.065;
+        const borderWidth = 0.017;
+        const lineCap = "butt";
+        const muscleIntensityAttributeName = "muscleIntensity";
+
+        ctx.beginPath();
+        ctx.lineCap = lineCap;
+        ctx.strokeStyle = borderColor;
+        ctx.lineWidth = (width + borderWidth * 2) * scale;
+        ctx.moveTo(a[0], a[1]);
+        ctx.lineTo(b[0], b[1]);
+        ctx.stroke();
+
+        ctx.beginPath();
+
+        const muscleIntensity = args.mesh.getCustomAttribute(muscleIntensityAttributeName);
+        if (muscleIntensity == null) {
+          throw new Error(`muscle intensity attribute (${muscleIntensityAttributeName}) not found, call setCustomAttribute("${muscleIntensityAttributeName}", value) before rendering.`);
+        }
+        if (!Array.isArray(muscleIntensity)) {
+          throw new Error(`muscle intensity attribute must be an array with values for each fiber, found ${typeof muscleIntensity}`);
+        }
+        
+        const t = muscleIntensity[muscleId];
+        
+        const cr0 = color0[0];
+        const cr1 = color1[0];
+
+        const cg0 = color0[1];
+        const cg1 = color1[1];
+
+        const cb0 = color0[2];
+        const cb1 = color1[2];
+
+        const cr = (1 - t) * cr0 + t * cr1;
+        const cg = (1 - t) * cg0 + t * cg1;
+        const cb = (1 - t) * cb0 + t * cb1;
+
+        ctx.strokeStyle = `rgb(${cr}, ${cg}, ${cb})`;
+        ctx.lineCap = lineCap;
+        ctx.lineWidth = width * scale;
+        ctx.moveTo(a[0], a[1]);
+        ctx.lineTo(b[0], b[1]);
+
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+module.exports = ViewportMuscles;
