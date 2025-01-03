5a2bf3b16e1bc8a9e7c94533ee907248d9c987c0
Junior Rojas <jrojasdavalos@gmail.com>
2025-01-02 19:31:58 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -e
+
 clang=${LLVM_BIN_DIR}/clang++
-llc=${LLVM_BIN_DIR}/llc
-ld=${LLVM_BIN_DIR}/wasm-ld
 opt=${LLVM_BIN_DIR}/opt
 
 lib_name="algovivo"
@@ -18,13 +18,17 @@ wasm_filename="${build_dirname}/${lib_name}.wasm"
 
 mkdir -p ${build_dirname}
 
-echo "compiling C++ to LLVM IR..." && \
-$clang --target=wasm32 -emit-llvm -c -S ${src_filename} -o ${ll_filename} && \
-echo "differentiating LLVM IR..." && \
-$opt ${ll_filename} -load=$ENZYME -enzyme -S -o ${ll_diff_filename} && \
-echo "optimizing differentiated LLVM IR..." && \
-$opt ${ll_diff_filename} -S -o ${ll_diff_opt_filename} && \
-echo "compiling LLVM IR to WASM..." && \
-$llc -march=wasm32 -filetype=obj -o ${o_filename} ${ll_diff_opt_filename} && \
-$ld --no-entry -allow-undefined --export-all -o ${wasm_filename} ${o_filename} && \
+echo "compiling C++ to LLVM IR..."
+$clang --target=wasm32 -emit-llvm -c -S ${src_filename} -o ${ll_filename}
+echo "differentiating LLVM IR..."
+$opt ${ll_filename} -load=$ENZYME -enzyme -S -o ${ll_diff_filename}
+echo "optimizing differentiated LLVM IR..."
+$opt ${ll_diff_filename} -S -o ${ll_diff_opt_filename}
+echo "compiling LLVM IR to WASM..."
+
+llc=${LLVM_BIN_DIR}/llc
+ld=${LLVM_BIN_DIR}/wasm-ld
+
+$llc -march=wasm32 -filetype=obj -o ${o_filename} ${ll_diff_opt_filename}
+$ld --no-entry -allow-undefined --export-all -o ${wasm_filename} ${o_filename}
 echo "saved to ${wasm_filename}"
