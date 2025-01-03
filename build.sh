819bc585859c264ed03000adbf515f2d20f8baf7
Junior Rojas <jrojasdavalos@gmail.com>
2025-01-02 19:37:18 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -13,21 +13,24 @@ build_dirname="${this_dirname}/build"
 ll_filename="${build_dirname}/${lib_name}.out.ll"
 ll_diff_filename="${build_dirname}/${lib_name}.diff.out.ll"
 ll_diff_opt_filename="${build_dirname}/${lib_name}.diff.opt.out.ll"
-o_filename="${build_dirname}/${lib_name}.out.o"
-wasm_filename="${build_dirname}/${lib_name}.wasm"
 
 mkdir -p ${build_dirname}
 
 echo "compiling C++ to LLVM IR..."
 $clang --target=wasm32 -emit-llvm -c -S ${src_filename} -o ${ll_filename}
+
 echo "differentiating LLVM IR..."
 $opt ${ll_filename} -load=$ENZYME -enzyme -S -o ${ll_diff_filename}
+
 echo "optimizing differentiated LLVM IR..."
 $opt ${ll_diff_filename} -S -o ${ll_diff_opt_filename}
+
 echo "compiling LLVM IR to WASM..."
 
 llc=${LLVM_BIN_DIR}/llc
 ld=${LLVM_BIN_DIR}/wasm-ld
+o_filename="${build_dirname}/${lib_name}.out.o"
+wasm_filename="${build_dirname}/${lib_name}.wasm"
 
 $llc -march=wasm32 -filetype=obj -o ${o_filename} ${ll_diff_opt_filename}
 $ld --no-entry -allow-undefined --export-all -o ${wasm_filename} ${o_filename}
