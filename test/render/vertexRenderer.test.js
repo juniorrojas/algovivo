6ed9af22c71df0ef1c51842e5a194ab0f312c2c0
Junior Rojas <jrojasdavalos@gmail.com>
2025-01-06 20:10:51 -0800

    Update test

--- test/render/vertexRenderer.test.js
+++ test/render/vertexRenderer.test.js
@@ -2,6 +2,6 @@ const algovivo = require("algovivo");
 
 test("vertex renderer", () => {
   const system = { numVertices: 4 };
-  const viewportVertices = new algovivo.render.VertexRenderer({ system });
-  expect(viewportVertices.numVertices).toBe(4);
+  const vertexRenderer = new algovivo.render.VertexRenderer({ system });
+  expect(vertexRenderer.numVertices).toBe(4);
 });
