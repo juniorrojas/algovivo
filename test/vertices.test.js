e6293e131c0b212aaef1b22ffb2681df29782256
Junior Rojas <jrojasdavalos@gmail.com>
2025-01-11 14:42:08 -0800

    Update tests

--- test/vertices.test.js
+++ test/vertices.test.js
@@ -5,6 +5,25 @@ expect.extend({ toBeCloseToArray: utils.toBeCloseToArray });
 
 test("set vertices", async () => {
   const ten = await utils.loadTen();
+  const vertices = new algovivo.Vertices({ ten });
+  expect(vertices.numVertices).toBe(0);
+
+  vertices.set([
+    [0, 0],
+    [3, 0],
+    [2, 4]
+  ]);
+
+  expect(vertices.numVertices).toBe(3);
+  expect(vertices.pos.toArray()).toBeCloseToArray([
+    [0, 0],
+    [3, 0],
+    [2, 4]
+  ]);
+});
+
+test("set system vertices", async () => {
+  const ten = await utils.loadTen();
   const system = new algovivo.System({ ten });
   expect(system.numVertices).toBe(0);
 
