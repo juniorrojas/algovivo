6236b01ff542b94b6f76d61c89171677a7146a63
Junior Rojas <jrojasdavalos@gmail.com>
2025-01-12 23:16:50 -0800

    Update tests (#211)

--- algovivo/index.js
+++ algovivo/index.js
@@ -1,10 +1,13 @@
 const System = require("./System");
+const Vertices = require("./Vertices");
+
 const mmgrten = require("./mmgrten/index");
 const render = require("./render");
 const mm2d = require("./render/mm2d");
 
 module.exports = {
   System: System,
+  Vertices: Vertices,
   mmgrten: mmgrten,
   SystemViewport: render.SystemViewport,
   mm2d: mm2d,
