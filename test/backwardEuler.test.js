9b4056ea6e4128e7097a112c8da3573fb0997f4d
Junior Rojas <jrojasdavalos@gmail.com>
2025-01-17 18:50:57 -0800

    Update test

--- test/backwardEuler.test.js
+++ test/backwardEuler.test.js
@@ -34,3 +34,35 @@ test("update vel", async () => {
     [-2, 5]
   ]);
 });
+
+test("optim init", async () => {
+  const ten = await utils.loadTen();
+
+  const numVertices = 3;
+  const spaceDim = 2;
+  
+  const pos0 = ten.tensor([
+    [1, 2],
+    [3, 4],
+    [5, 6]
+  ]);
+  const vel0 = ten.tensor([
+    [0.5, 4],
+    [1, -0.5],
+    [-2, 5]
+  ]);
+  const pos = ten.zeros([numVertices, spaceDim]);
+  const dt = 2;
+
+  ten.wasmInstance.exports.optim_init(
+    numVertices, spaceDim,
+    dt,
+    pos0.ptr, vel0.ptr,
+    pos.ptr
+  );
+  expect(pos.toArray()).toBeCloseToArray([
+    [2, 10],
+    [5, 3],
+    [1, 16]
+  ]);
+});
