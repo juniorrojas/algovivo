2e786b465c5a18108935680ebd25682728a5b62c
Junior Rojas <jrojasdavalos@gmail.com>
2025-04-02 20:41:19 -0700

    WIP

--- utils/codegen/algovivo_codegen/backward_euler.py
+++ utils/codegen/algovivo_codegen/backward_euler.py
@@ -1,5 +1,36 @@
 from .codegen import Fun, indent
 
+class Muscles:
+    def __init__(self):
+        pass
+
+    def add_to_loss(self, be):
+        be.loss_body += """
+for (int i = 0; i < num_muscles; i++) {
+  const auto offset = i * 2;
+  const auto i1 = muscles[offset    ];
+  const auto i2 = muscles[offset + 1];
+
+  accumulate_muscle_energy(
+    potential_energy,
+    pos,
+    i1, i2,
+    a[i], l0[i], k
+  );
+}"""
+
+class Triangles:
+    def __init__(self):
+        pass
+
+    def add_to_loss(self, be):
+        from .neohookean import Neohookean
+        neohookean = Neohookean(
+            simplex_order=3,
+            simplex_name_singular="triangle"
+        )
+        be.loss_body += "\n" + neohookean.codegen_accumulate_simplices_energy()
+
 class Collision:
     def __init__(self):
         pass
@@ -62,12 +93,13 @@ for (int i = 0; i < num_vertices; i++) {
   );
 }
 """
-    
 
 class BackwardEuler:
     def __init__(self):
         self.loss = Fun("backward_euler_loss")
         self.potentials = [
+            Muscles(),
+            Triangles(),
             Gravity(),
             Collision(),
             Friction()
@@ -90,8 +122,6 @@ float inertial_energy = 0.0;
 float potential_energy = 0.0;"""
 
         self.add_inertia()
-        self.add_muscles()
-        self.add_triangles()
 
         for potential in self.potentials:
             potential.add_to_loss(self)
@@ -141,26 +171,3 @@ for (int i = 0; i < num_vertices; i++) {
     space_dim
   );
 }"""
-
-    def add_muscles(self):
-        self.loss_body += """
-for (int i = 0; i < num_muscles; i++) {
-  const auto offset = i * 2;
-  const auto i1 = muscles[offset    ];
-  const auto i2 = muscles[offset + 1];
-
-  accumulate_muscle_energy(
-    potential_energy,
-    pos,
-    i1, i2,
-    a[i], l0[i], k
-  );
-}"""
-
-    def add_triangles(self):
-        from .neohookean import Neohookean
-        neohookean = Neohookean(
-            simplex_order=3,
-            simplex_name_singular="triangle"
-        )
-        self.loss_body += "\n" + neohookean.codegen_accumulate_simplices_energy()
