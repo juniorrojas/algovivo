f55719952fe3b84e62ee2842d66fbd89c45b481a
Junior Rojas <jrojasdavalos@gmail.com>
2025-08-17 01:40:48 -0700

    Update csrc

--- csrc/modules/collision.h
+++ csrc/modules/collision.h
@@ -5,9 +5,9 @@ namespace algovivo {
 __attribute__((always_inline))
 void accumulate_collision_energy(
   float &energy,
-  float py
+  float py,
+  float k_collision
 ) {
-  const float k_collision = 14000.0;
   if (py < 0) {
     energy += k_collision * py * py;
   }
