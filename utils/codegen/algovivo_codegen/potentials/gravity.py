f3c67e192846abfdf4a842965e9af9155e508d51
Junior Rojas <jrojasdavalos@gmail.com>
2025-10-23 21:48:20 -0500

    Update codegen utils

--- utils/codegen/algovivo_codegen/potentials/gravity.py
+++ utils/codegen/algovivo_codegen/potentials/gravity.py
@@ -1,9 +1,11 @@
+from .. import Fun
+
 class Gravity:
     def __init__(self):
         pass
 
-    def add_to_loss(self, be):
-        be.loss_body += """
+    def get_src(self):
+        return """
 for (int i = 0; i < num_vertices; i++) {
   const auto offset = space_dim * i;
 
@@ -18,3 +20,14 @@ for (int i = 0; i < num_vertices; i++) {
   );
 }
 """
+
+    def make_loss_fn(self, name):
+        f = Fun(name)
+        f.args.add_arg("int", "space_dim")
+        f.args.add_arg("float", "g")
+        f.args.add_arg("int", "num_vertices")
+        f.src_body = self.get_src()
+        return f
+
+    def add_to_loss(self, be):
+        be.loss_body += self.get_src()
