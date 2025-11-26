97f924a7aa41378f0e132017b36c5a438c68e5e3
Junior Rojas <jrojasdavalos@gmail.com>
2025-11-25 18:43:49 -0800

    Update browser utils (#287)

--- utils/trajectory/ppw/Window.js
+++ utils/trajectory/ppw/Window.js
@@ -17,7 +17,8 @@ class Window {
     const browser = this.browser = await puppeteer.launch({
       headless: this.headless,
       args: [
-        `--window-size=${this.width},${this.height}`
+        `--window-size=${this.width},${this.height}`,
+        "--no-sandbox"
       ]
     });
     const page = await browser.newPage();
