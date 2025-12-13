084d0571b946b849da3d33830ad2ddce7d9276ac
Junior Rojas <jrojasdavalos@gmail.com>
2025-12-12 20:40:12 -0800

    Update test

--- test/frameProjection.test.js
+++ test/frameProjection.test.js
@@ -1,6 +1,9 @@
 const algovivo = require("algovivo");
 const mmgrten = algovivo.mmgrten;
 const { loadWasm, loadTen } = require("./utils");
+const utils = require("./utils");
+
+expect.extend({ toBeCloseToArray: utils.toBeCloseToArray });
 
 test("dot2d", async () => {
   const wasmInstance = await loadWasm();
@@ -11,7 +14,7 @@ test("dot2d", async () => {
   expect(r).toBe(11);
 });
 
-test("frame_projection", async () => {
+test("frame_projection simple", async () => {
   const ten = await loadTen();
 
   const numVertices = 2;
@@ -43,6 +46,50 @@ test("frame_projection", async () => {
   ]);
 });
 
+test("frame_projection", async () => {
+  const ten = await loadTen();
+
+  const x = ten.tensor([
+    [-1.3, 2.7],
+    [2.3, 0.9],
+    [1, 1.3]
+  ]);
+
+  const numVertices = x.shape.get(0);
+  const spaceDim = x.shape.get(1);
+
+  const projectedX = ten.zeros([numVertices, spaceDim]);
+
+  const centerId = 1;
+  const forwardId = 2;
+
+  const subtractPositon = true;
+  const clockwise = true;
+
+  ten.wasmInstance.exports.frame_projection(
+    numVertices,
+    x.ptr,
+    centerId,
+    forwardId,
+    x.ptr,
+    projectedX.ptr,
+    subtractPositon,
+    clockwise
+  );
+
+  const result = projectedX.toArray();
+  console.log(result);
+
+  expect(result).toBeCloseToArray([
+    [3.9701590538024902, 0.6616933345794678],
+    [0, 0],
+    [1.3601469993591309, 2.9802322387695312e-8]
+  ]);
+  
+  x.dispose();
+  projectedX.dispose();
+});
+
 test("make_policy_input", async () => {
   const ten = mmgrten.engine({
     wasmInstance: await loadWasm()
@@ -50,7 +97,7 @@ test("make_policy_input", async () => {
 
   const numVertices = 3;
   const spaceDim = 2;
-  
+
   const x = ten.tensor([
     [1, 2],
     [3, 4],
