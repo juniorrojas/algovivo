6fb727f5c4656c11969238a870d7ce1cdc7dcfde
Junior Rojas <jrojasdavalos@gmail.com>
2026-05-15 20:42:37 -0500

    Update demo

--- demo/rollup.config.js
+++ demo/rollup.config.js
@@ -1,4 +1,17 @@
 import terser from "@rollup/plugin-terser";
+import crypto from "crypto";
+import fs from "fs";
+
+function emitReleaseJson() {
+  return {
+    name: "emit-release-json",
+    writeBundle() {
+      const bundle = fs.readFileSync("./public/main.build.js");
+      const id = crypto.createHash("sha256").update(bundle).digest("hex").slice(0, 12);
+      fs.writeFileSync("./public/release.build.json", JSON.stringify({ id }));
+    }
+  };
+}
 
 export default {
   input: ["src/main.js"],
@@ -8,6 +21,7 @@ export default {
     sourcemap: false
   },
   plugins: [
-    terser()
+    terser(),
+    emitReleaseJson()
   ],
 };
