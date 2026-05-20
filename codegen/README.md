a52c55da317895054c70e61dbe5599ee473ae2de
Junior Rojas <jrojasdavalos@gmail.com>
2026-05-19 23:25:54 -0500

    Update codegen utils (#340)

---
+++ codegen/README.md
@@ -0,0 +1,11 @@
+# algovivo codegen
+
+To use algovivo's codegen library, you can install the algovivo-codegen skill:
+
+```sh
+npx skills add https://github.com/juniorrojas/algovivo/tree/main/codegen/skills/algovivo-codegen
+```
+
+Then, tell your agent:
+
+> Initialize an algovivo codegen project to define energy functions from scratch in Python. Generate the C++ and compile it to WASM.
--- codegen/algovivo_codegen/potentials/muscles.py
+++ codegen/algovivo_codegen/potentials/muscles.py
@@ -1,9 +1,11 @@
+from .. import Fun
+
 class Muscles:
     def __init__(self):
         pass
 
-    def add_to_loss(self, be):
-        be.loss_body += """
+    def get_src(self):
+        return """
 for (int i = 0; i < num_muscles; i++) {
   const auto offset = i * 2;
   const auto i1 = muscles[offset    ];
@@ -15,4 +17,19 @@ for (int i = 0; i < num_muscles; i++) {
     i1, i2,
     a[i], l0[i], k
   );
-}"""
+}
+"""
+
+    def make_energy_fn(self, name="muscle_energy"):
+        f = Fun(name)
+        f.args.add_arg("float", "k")
+        f.args.add_arg("int", "num_muscles")
+        f.args.add_arg("int*", "muscles")
+        f.args.add_arg("float*", "a")
+        f.args.add_arg("float*", "l0")
+        f.args.add_arg("float*", "pos", differentiable=True, size="num_vertices * space_dim")
+        f.src_body = "float potential_energy = 0.0;" + self.get_src() + "return potential_energy;"
+        return f
+
+    def add_to_loss(self, be):
+        be.loss_body += self.get_src()
