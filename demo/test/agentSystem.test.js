b468c77d3a2f2d4712bf796636b4c135b2cdb41d
Junior Rojas <jrojasdavalos@gmail.com>
2026-09-23 00:39:02 -0700

    Use ESM build in test

--- demo/test/agentSystem.test.js
+++ demo/test/agentSystem.test.js
@@ -1,6 +1,6 @@
 import fsp from "fs/promises";
 import AgentSystem from "../src/AgentSystem.js";
-import * as algovivo from "../../build/algovivo.cjs";
+import * as algovivo from "../../build/algovivo.js";
 import { loadWasm } from "./utils.js";
 
 test("agent system", async () => {
